d8523f7027baf8956c4c36fbfd82c5575a511df7
Nafisa Ahmed <ahmednafisa11@gmail.com>
2022-10-12 16:26:57 +0600

    eyeIcon and its toast added

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Question = ({ quiz }) => {
     const { question, options, correctAnswer } = quiz;
+    const notify = () => toast(`Correct Answer: ${correctAnswer}`);
+
 
     return (
-        <div className='text-center'>
-            <h1 className='mt-5 text-violet-700 font-bold'>Quiz: {question.replace(/(<([^>]+)>)/ig, '')}</h1>
+        <div className='text-center drop-shadow-2xl my-10 mx-auto bg-slate-100 w-8/12 p-5'>
+            <div className='flex mx-44'>
+                <h1 className='mt-5 text-violet-700 font-bold'>Quiz: {question.replace(/(<([^>]+)>)/ig, '')}</h1>
+                <button onClick={notify} className='ml-12 my-5'><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></button>
+            </div>
             <form action="">
-                <div className='grid grid-cols-2 mx-80'>
+                <div className='grid grid-cols-2 px-24'>
                     <div className='mt-5'>
                         <div className='border-2 py-4 m-2 hover:bg-slate-200'>
                             <input onClick={() => options[0] === correctAnswer ? toast('right') : toast('wrong')} className='mr-3' type="radio" name="choose" id="a" value={options[0] ? options[0] : 'null'} />
